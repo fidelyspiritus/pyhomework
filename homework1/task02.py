f0ab1bc837deec6af9360f1d3b0f686afa0cb03d
Anastasiia <79062589+fidelyspiritus@users.noreply.github.com>
2021-03-08 17:35:06 +0300

    Проверка на длину последовательности

--- homework1/task02.py
+++ homework1/task02.py
@@ -10,6 +10,8 @@ from typing import Sequence
 
 
 def check_fibonacci(data: Sequence[int]) -> bool:
+    if len(Sequence) < 3:
+        return False
     while len(Sequence) >= 3:
         first, second, third = Sequence[0], Sequence[1], Sequence[2]
         if third != first + second:
