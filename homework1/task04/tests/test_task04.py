6a2c663c52f2d26d351293d2b8744391c566917e
Anastasiia <anastasiia.a.markova@gmail.com>
2021-03-11 00:46:17 +0300

    hw1 test
    almost

--- homework1/task04/tests/test_task04.py
+++ homework1/task04/tests/test_task04.py
@@ -1,5 +1,19 @@
+from typing import List
+
+import pytest
+
 from homework1.task04 import check_sum_of_four
 
 
-def test_main():
-    assert True
+@pytest.mark.parametrize(
+    ["value", "expected_result"],
+    [
+        (([1, 2], [1, 2], [-2, 1], [-3, -4]), 2),
+        (([0, 0], [0, 0], [0, 0], [0, 0]), 2 ** 4),
+    ],
+)
+def test_check_sum_of_four(
+    value: (List[int], List[int], List[int], List[int]), expected_result: int
+):
+    actual_result = check_sum_of_four(value)
+    assert actual_result == expected_result
--- homework1/task05/tests/test_task05.py
+++ homework1/task05/tests/test_task05.py
@@ -1,5 +1,16 @@
+from typing import List
+
+import pytest
+
 from homework1.task05 import find_maximal_subarray_sum
 
 
-def test_main():
-    assert True
+@pytest.mark.parametrize(
+    ["value", "parameter", "expected_result"],
+    [
+        ([1, 3, -1, -3, 5, 3, 6, 7], 3, 16),
+    ],
+)
+def test_find_maximal_subarray_sum(value: List[int], int, expected_result: int):
+    actual_result = find_maximal_subarray_sum(value)
+    assert actual_result == expected_result
